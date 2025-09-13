5a046c28c557d62026e4ef242e705978186dda79
Pranesh <praneshwaran574@gmail.com>
2025-09-13 15:41:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/First and Second Smallests/first-and-second-smallests.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minAnd2ndMin(self, arr):
+        # code here
+        arr = list(set(arr))
+        arr.sort()
+        
+        if len(arr) == 1:
+            return [-1]
+            
+        return [arr[0],arr[1]]
+        
