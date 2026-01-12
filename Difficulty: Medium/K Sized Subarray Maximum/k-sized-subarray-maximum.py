6bd50c7d51a68d05c3a56962da26ff3b6750336a
Pranesh <praneshwaran574@gmail.com>
2026-01-12 23:21:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxOfSubarrays(self, arr, k):
+        # code here
+        n = len(arr)
+        res = []
+        q = deque()
+        
+        for i in range(n):
+            
+            if q and q[0] <= i - k:
+                q.popleft()
+                
+            while q and arr[q[-1]] < arr[i]:
+                q.pop()
+                
+            q.append(i)
+            if i >= k - 1:
+                res.append(arr[q[0]])
+                
+        return res
+        
