b4dcaea67f54099542d30eacf25232be4e191864
Pranesh <praneshwaran574@gmail.com>
2026-02-16 19:51:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Meeting Rooms/meeting-rooms.py
@@ -0,0 +1,13 @@
+class Solution:
+    def canAttend(self, arr):
+        # Code Here
+        arr.sort(key = lambda x: x[0])
+        #print(arr)
+        
+        for i in range(1,len(arr)):
+            if arr[i-1][1] > arr[i][0]:
+                return False
+                
+        return True
+                
+            
