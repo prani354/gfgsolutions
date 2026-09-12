87e95b8e47d8c2bc9ef4f1072f74a2b05d11186e
Pranesh <praneshwaran574@gmail.com>
2026-09-12 12:22:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.py
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.py
@@ -2,10 +2,17 @@ class Solution:
     def knapsack(self, W: int, val: list[int], wt: list[int]) -> int:
         # code here
         n = len(val)
-        dp = [0] * (W+1)
+        dp = [[0] * (W+1) for _ in range(n+1)]
         
-        for i in range(n):
-            for w in range(W,wt[i]-1,-1):
-                dp[w] = max(dp[w],dp[w-wt[i]]+val[i])
+        for i in range(1,n+1):
+            for j in range(1,W+1):
                 
-        return dp[W]
+                if wt[i-1] <= j:  #It fits inside
+                    dp[i][j] = max(dp[i-1][j],dp[i-1][j-wt[i-1]] + val[i-1])
+                    
+                else:
+                    dp[i][j] = dp[i-1][j]
+                    
+                    
+        return dp[n][W]
+                    
